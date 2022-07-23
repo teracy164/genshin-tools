c04495f83478979ca1135dd0891fc6df66f6d7e5
teracy164 <literacy1945@gmail.com>
2022-07-23 18:01:03 +0900

    fix base_url for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  app: {
+    baseURL: process.env.BASE_URL,
+  },
+});
