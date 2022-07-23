aa24200df30ad6cf1e1c4071e9ff3a11e68c51b9
teracy164 <literacy1945@gmail.com>
2022-07-24 08:41:20 +0900

    nuxt.config.ts fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,5 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    baseURL: process.env.BASE_URL,
-    cdnURL: '/genshin-tools/',
-  },
   css: ['@/assets/css/styles.scss'],
 });
