cd1491b175c04a13b3b6467f39226f4b9898658e
teracy164 <literacy1945@gmail.com>
2022-07-23 22:35:15 +0900

    最低限の機能追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <a target="_blank" href="https://genshin-dictionary.com/opendata/">https://genshin-dictionary.com/opendata/</a>
     <div>
         <h1>Total Score</h1>
-        XXX.XX
+        {{ totalScore }}
     </div>
 
     <div>
@@ -12,22 +12,22 @@
             <div class="artifact">
                 <div class="artifact-type">
                     <h3>花</h3>
-                    <span class="score">xx.xx</span>
+                    <span class="score">{{ artifacts.flower.score }}</span>
                 </div>
                 <PickArtifacts :value="artifacts.flower.id" @chage="artifacts.flower.id = $event" />
                 <div class="sub-options">
-                    <div class="sub-option" v-for="op of artifacts.flower.sub">
-                        <label>
-                            <a @click="op.selected = !op.selected">{{ op.name }}</a>
+                    <div class="sub-option" :class="{ selected: op.selected }" v-for="op of artifacts.flower.sub">
+                        <label @click="op.selected = !op.selected">
+                            <a>{{ op.name }}</a>
                         </label>
-                        <input v-model="op.score" :disabled="!op.selected" />
+                        <input v-model="op.score" :disabled="!op.selected" @input="changeSubOption(artifacts.flower)" />
                     </div>
                 </div>
             </div>
             <div class="artifact">
                 <div class="artifact-type">
                     <h3>羽</h3>
-                    <span class="score">xx.xx</span>
+                    <span class="score">{{ artifacts.plume.score }}</span>
                 </div>
                 <PickArtifacts :value="artifacts.plume.id" @chage="artifacts.plume.id = $event" />
                 <div class="sub-options">
@@ -35,14 +35,14 @@
                         <label>
                             <a @click="op.selected = !op.selected">{{ op.name }}</a>
                         </label>
-                        <input v-model="op.score" :disabled="!op.selected" />
+                        <input v-model="op.score" :disabled="!op.selected" @input="changeSubOption(artifacts.plume)" />
                     </div>
                 </div>
             </div>
             <div class="artifact">
                 <div class="artifact-type">
                     <h3>時計</h3>
-                    <span class="score">xx.xx</span>
+                    <span class="score">{{ artifacts.eon.score }}</span>
                 </div>
                 <PickArtifacts :value="artifacts.eon.id" @chage="artifacts.eon.id = $event" />
                 <div class="sub-options">
@@ -50,14 +50,14 @@
                         <label>
                             <a @click="op.selected = !op.selected">{{ op.name }}</a>
                         </label>
-                        <input v-model="op.score" :disabled="!op.selected" />
+                        <input v-model="op.score" :disabled="!op.selected" @input="changeSubOption(artifacts.eon)" />
                     </div>
                 </div>
             </div>
             <div class="artifact">
                 <div class="artifact-type">
                     <h3>杯</h3>
-                    <span class="score">xx.xx</span>
+                    <span class="score">{{ artifacts.goblet.score }}</span>
                 </div>
                 <PickArtifacts :value="artifacts.goblet.id" @chage="artifacts.goblet.id = $event" />
                 <div class="sub-options">
@@ -65,14 +65,14 @@
                         <label>
                             <a @click="op.selected = !op.selected">{{ op.name }}</a>
                         </label>
-                        <input v-model="op.score" :disabled="!op.selected" />
+                        <input v-model="op.score" :disabled="!op.selected" @input="changeSubOption(artifacts.goblet)" />
                     </div>
                 </div>
             </div>
             <div class="artifact">
                 <div class="artifact-type">
                     <h3>冠</h3>
-                    <span class="score">xx.xx</span>
+                    <span class="score">{{ artifacts.circlet.score }}</span>
                 </div>
                 <PickArtifacts :value="artifacts.circlet.id" @chage="artifacts.circlet.id = $event" />
                 <div class="sub-options">
@@ -80,7 +80,8 @@
                         <label>
                             <a @click="op.selected = !op.selected">{{ op.name }}</a>
                         </label>
-                        <input v-model="op.score" :disabled="!op.selected" />
+                        <input v-model="op.score" :disabled="!op.selected"
+                            @input="changeSubOption(artifacts.circlet)" />
                     </div>
                 </div>
             </div>
@@ -104,39 +105,30 @@ const subOptions = [
     { id: 'def', ja: '防御力', en: 'DEF' },
     { id: 'dr', ja: '防御力%', en: 'DEF%' },
 ];
-type Artifact = { id: string; level: number; sub: { id: string; name: string; score: number; selected: boolean; }[] };
-const artifacts: { [key: string]: Artifact } = {
-    flower: {
-        id: '',
-        level: 0,
-        sub: [],
-    },
-    plume: {
-        id: '',
-        level: 0,
-        sub: [],
-    },
-    eon: {
-        id: '',
-        level: 0,
-        sub: [],
-    },
-    goblet: {
-        id: '',
-        level: 0,
-        sub: [],
-    },
-    circlet: {
-        id: '',
-        level: 0,
-        sub: [],
-    },
-}
+type SubOption = { id: string; name: string; score: number; selected: boolean; }
+type Artifact = { id: string; level: number; score: number; sub: SubOption[] };
+const totalScore = useState('totalScore', () => 0);
+const artifacts = useState<{ [key: string]: Artifact }>('artifacts', () => {
+    const data = {
+        flower: { id: '', level: 0, score: 0, sub: [] },
+        plume: { id: '', level: 0, score: 0, sub: [] },
+        eon: { id: '', level: 0, score: 0, sub: [] },
+        goblet: { id: '', level: 0, score: 0, sub: [] },
+        circlet: { id: '', level: 0, score: 0, sub: [] },
+    }
 
-Object.values(artifacts).forEach(artifact => {
-    subOptions.forEach(op => artifact.sub.push({ id: op.id, name: op[$language.selected], score: null, selected: false }))
-})
+    Object.values(data).forEach(artifact => {
+        subOptions.forEach(op => artifact.sub.push({ id: op.id, name: op[$language.selected], score: null, selected: false }))
+    })
+    return data
+});
 
+const changeSubOption = (artifact: Artifact) => {
+    const targets = { cr: 2, cd: 1, ar: 1 }
+    artifact.score = artifact.sub.reduce((total, op) => total + (op.score || 0) * (targets[op.id] || 0), 0);
+
+    totalScore.value = Object.values(artifacts.value).reduce((total, artifact) => total + artifact.score, 0);
+};
 </script>
 <style lang="scss">
 .artifact {
@@ -156,10 +148,14 @@ Object.values(artifacts).forEach(artifact => {
         }
 
         .score {
-            border: 1px solid #c0c0c0;
+            background-color: #ffcccc;
+            box-shadow: 1px 1px 3px lightgray;
             border-radius: 20px;
             margin: 0 10px;
             padding: 0 5px;
+            min-width: 1.5em;
+            line-height: 2em;
+            text-align: center;
 
             &.good {
                 background-color: #ccccff;
@@ -183,19 +179,26 @@ Object.values(artifacts).forEach(artifact => {
         .sub-option {
             width: 100%;
             display: flex;
+            margin-bottom: 3px;
 
             label {
                 flex-basis: 100%;
+                cursor: pointer;
+            }
+
+            label:hover {
+                background-color: #eeeeee;
             }
 
             input {
                 width: 5em;
             }
+
+            &.selected {
+                background-color: #ffcccc;
+            }
         }
     }
 
-    .sub-option:hover {
-        background-color: #eeeeee;
-    }
 }
 </style>
