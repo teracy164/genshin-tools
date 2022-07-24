b17a84ed988eecc1a120bcfdc46d48037dde8618
teracy164 <teracy164@users.noreply.github.com>
2022-07-24 09:00:56 +0000

    deploy: f270934063c5b0dfb9a53ab8f54ffc8c837c05b1

---
+++ _nuxt/error-component.1936fa70.mjs
@@ -0,0 +1,1 @@
+import{d as n,u as a,o,c,a as e,t as s,b as r}from"./entry.df4e5722.mjs";const l={style:{height:"100%",display:"flex","justify-content":"center","align-items":"center"}},d=n({__name:"error",setup(i){const t=a();return(u,p)=>(o(),c("div",l,[e("div",null,[e("p",null,s(r(t).statusCode),1),e("p",null,s(r(t).message),1)])]))}});export{d as default};
