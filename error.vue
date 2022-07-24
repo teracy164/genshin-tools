b5f69e774d617d592059b07d64f113ede020ad66
teracy164 <literacy1945@gmail.com>
2022-07-24 16:22:14 +0900

    エラーページ追加

---
+++ error.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const error = useError()
+</script>
+
+<template>
+    <div style="height: 100%;display: flex;justify-content: center;align-items: center;">
+        <div>
+            <p>{{ error.statusCode }}</p>
+            <p>{{ error.message }}</p>
+        </div>
+    </div>
+</template>
