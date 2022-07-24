b6c818a4a9335d063d0e7a86a21984572730dffd
teracy164 <literacy1945@gmail.com>
2022-07-24 16:46:46 +0900

    cd fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,8 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   app: {
-    cdnURL: process.env.BASE_URL,
     head: {
-      link: [{ rel: 'icon', type: 'image/png', href: (process.env.BASE_URL || '') + '/assets/favicon.ico' }],
+      link: [{ rel: 'icon', type: 'image/png', href: '/assets/favicon.ico' }],
     },
   },
   css: ['@/assets/css/styles.scss'],
