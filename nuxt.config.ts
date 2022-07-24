806ac9e614ab1e501b80b2119e06fe7a65b740c2
teracy164 <literacy1945@gmail.com>
2022-07-24 16:28:48 +0900

    config fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 import { defineNuxtConfig } from 'nuxt';
-console.log('BASE URL', process.env.BASE_URL);
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: { cdnURL: process.env.BASE_URL },
+  app: {
+    cdnURL: process.env.BASE_URL,
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: (process.env.BASE_URL || '') + '/favicon.ico' }],
+    },
+  },
   css: ['@/assets/css/styles.scss'],
 });
