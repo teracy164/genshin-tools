88497feb40b4acf14f05b46ab49ceb0a347301b7
teracy164 <literacy1945@gmail.com>
2022-07-24 15:37:46 +0900

    config.ts fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 import { defineNuxtConfig } from 'nuxt';
-
+console.log('BASE URL', process.env.BASE_URL);
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    cdnURL: process.env.BASE_URL,
-  },
+  app: { baseURL: process.env.BASE_URL },
   css: ['@/assets/css/styles.scss'],
 });
